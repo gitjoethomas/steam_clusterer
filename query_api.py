ba65adc81dd5a4c33ecf9c2c2e66a601ba0dc835
Joe <josephwthomas@hotmail.co.uk>
2019-09-02 22:14:33 +0100

    building query class

--- query_api.py
+++ query_api.py
@@ -16,157 +16,91 @@ import requests
 import keyring
 from IPython.display import clear_output
 
-
-# In[3]:
-
-
-# keyring.set_password('steam',username = 'IcyJoseph',password = 'E0152C2E7BF8A5AA0361A4DADCCE2287') # password config
-# keyring.get_password('steam', 'IcyJoseph')
-
-
-# In[18]:
-
-
-number_ids_you_want = 1000000 # how many unique steam ids do you want? the API queryer will stop when it has more than
-                              # this many
-
-
-# In[5]:
-
-
-password = keyring.get_password(service_name = 'steam', username = 'IcyJoseph')
-steam_id = '76561198985648791' # this is my steam_id
-new_id = '76561198020258797' # this is an ID I got from reddit - here:
-#                             https://www.reddit.com/r/Steam/comments/68yr8k/building_a_big_list_of_steam_ids_hit_me_up_if_you/
-
-
-# In[6]:
-
-
-file = open('steam_ids', mode = 'r')# read in file
-user_ids = file.read()
-user_ids = user_ids.replace(" ","") # cleaning
-user_ids = user_ids[1:-1].split(",")
-user_ids = pd.DataFrame({'user_id':user_ids})
-
-# user_ids.head()
-
-
-# In[7]:
-
-
-def get_player_info(steam_id):
+class QueryApi():
+    '''
+    Contains several functions for querying steam's api. Requires an API key to instantiate
     
-    response = requests.get(
-        f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={password}&steamids={steam_id}')
-    return response
-
-
-# In[8]:
-
-
-def get_friends(steam_id):
-    # returns basic profile information
-    response = requests.get(
-        f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={password}&steamid={steam_id}&relationship=friend')
-    return response
-
-
-# In[9]:
-
-
-def get_games(steam_id):
-    # returns basic profile information
-    response = requests.get(
-        f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={password}&steamid={steam_id}&format=json')
-    return response
-
-
-# In[10]:
-
-
-# me = get_player_info(steam_id)
-# friends = get_friends(steam_id)
-# games = get_games(steam_id)
-
-
-# In[11]:
-
-
-# me.content # my profile info
-
-
-# In[12]:
-
-
-# friends.content # i have no friends
-
-
-# In[13]:
-
-
-# games.content # i only have one game. need to get a lookup from appid to name of game (it's middle earth: shadow of mordor)
-
-
-# In[14]:
-
-
-# next steps:
-    # get list of active steam_ids (scraping forum?)
-    # find a lookup from appid to game name
-    # create function that takes friend's steam_id as an input and returns their top (5? 10?) played games
-    # pull some sort of info on players
-
-
-# In[15]:
-
-
-# takes a steam_id and returns all the id's friends
-def find_all_friends(steam_id):
+    get_player_info(steam_id) - basic info on a player. Returns a response object
+    get_friends(steam_id) - gets all public friends for a steam id. the id you supply can actually be a list of ids, separated by commas
+    find_all_friends(steam_id) - uses the above function and separates the steam ids into a list, and returns that
+    get_games(steam_id) - returns all games played by the steam_id, returns a response object
+    users_games(steam_id) - pulls out the names of the games played by a single steam_id. Pretty slow
+    '''
     
-    friends = get_friends(steam_id) # query steam api
-
-    splitfriends = str(friends.content).split('"')
-    all_friends = [i for i in splitfriends if i.isnumeric() == True] # items which are all numeric are steam_ids
-    
-    return all_friends
-
-
-# In[ ]:
-
-
-# need to get a list of active steam ids. 
-# I'm pulling friends own a known popular ID, then pulling friends of each friend, and so on until I get 1M IDs
-
-counter_start = 0
-all_ids = list()
-all_ids.append(new_id)
-
-while len(all_ids) < number_ids_you_want and counter_start != len(all_ids):
-    
-    counter_end = counter_start+100 # the steam api can accept up to 100 steam ids at once, so we'll give it 100...
-    
-    if counter_end > len(all_ids): # ... or as many as we can if we don't have 100
-        counter_end = len(all_ids)
+    def __init__(self, api_key):
         
-    id_string = [i+"," for i in all_ids[counter_start:counter_end]] # pull out the ids from our list
-    ids = "".join(id_string) # and concatenate them all into one string
+        self.api_key = api_key
+        
+        response = requests.get(f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={self.api_key}&steamids=blank')
+        
+        if response:
+            print("Successfully connected to the Steam API")
+        else:
+            print("Connection unsuccessful - API key rejected")
     
-    steam_ids = find_all_friends(ids)
+
+    def get_player_info(self, steam_id):
+        'Returns basic info on a player, in a response object'
+        
+        response = requests.get(
+            f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={self.api_key}&steamids={steam_id}')
+        
+        return response
     
-    all_ids.extend(steam_ids) # adds the friends pulled to the list of ids to iterate through, so we can keep pulling friends
-    all_ids = set(all_ids)
-    all_ids = list(all_ids)
     
-    clear_output()
-    print("all_ids is "+str(len(all_ids))+" items long")
+#     def get_game_info(self, app_id)
+        
+#         'Returns info on a game/app - requires an app id'
+        
+#         response = requests.get(
+#             f'http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001/?format=xml&appid={appid}&count=1&name[0]=global.map.emp_isle'
+            
+#         return response
+
+    def get_friends(self, steam_id):
+        
+        'gets all friend-related data for a steam id. the id you supply can actually be a list of ids, separated by commas'
+        
+        response = requests.get(
+            f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={self.api_key}&steamid={steam_id}&relationship=friend')
+        
+        return response
     
-    counter_start = counter_end
+    
+    def find_all_friends(self, steam_id):
 
+        'returns a list of all friends for a steam_id. Uses the get_friends() function.'
+        
+        friends = get_friends(steam_id) # query steam api
 
-# In[17]:
+        splitfriends = str(friends.content).split('"')
+        all_friends = [i for i in splitfriends if i.isnumeric() == True] # items which are all numeric are steam_ids
 
+        return all_friends
+    
+    
+    def get_users_games(self, steam_id):
+        
+        'returns a list of all games played by a user. This is very slow - needs work'
+        
+        response = requests.get(
+            f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={self.api_key}&steamid={steam_id}&format=json')
 
-file = open('all_steam_ids', mode = 'w+') # in the future, the lookup will just be a function i'll import and call
-file.write(str(all_ids))
+        gametext = get_games(steam_id)
 
+        # re.finditer returns an iterator object, so we have to use list comprehension to get the index at which the game 
+        # names begin
+        gamestart = [i.end() for i in re.finditer('name":"',str(response.content))]
+
+        gamelist = []
+
+        for start_index in gamestart: # we're pulling out the names of games for each user_id. This is very slow - needs work
+
+            end_index =  re.search('",',str(newgames.content)[start_index:])
+            end_index = end_index.start()
+
+            game = str(newgames.content)[start_index:start_index+end_index]
+
+            gamelist.append(game)
+
+        return gamelist
